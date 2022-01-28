dad82f3b5f128307549a0ab019412e1006cb95e7
Quincy Chang <quincy0710@hotmail.com>
2022-01-28 18:03:07 +0800

    feat(tester): add cardano page

--- packages/coin-tester/src/components/coins/ada/index.tsx
+++ packages/coin-tester/src/components/coins/ada/index.tsx
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 import { Transport, apdu, utils, config } from '@coolwallet/core';
 import { NoInput, TwoInputs } from '../../../utils/componentMaker';
 
-import ADA from '@coolwallet/ada';
+import ADA, { TransferWithoutFee, Options } from '@coolwallet/ada';
 
 interface Props {
   transport: Transport | null,
@@ -13,9 +13,31 @@ interface Props {
   setIsLocked: (isLocked:boolean) => void,
 }
 
+const txWithoutFee = {
+  signers: [{
+    rolePath: 0,
+    indexPath: 0,
+  }],
+  inputs: [{
+    txId: '0x8561258e210352fba2ac0488afed67b3427a27ccf1d41ec030c98a8199bc22ec',
+    index: 0,
+  }],
+  output: {
+    address: 'addr1qxn5anyxv6dhtl57yvgvpp25emy0pc9wenqzzemxktyr94ahaaap0f0tn4wxaqsydnzty2m0y4gfeu39ckjvsjycs4nssxhc25',
+    amount: 10523059,
+  },
+  change: {
+    address: 'addr1q8wyqhxud34ejxjm5tyj74qeuttr7z9vnjuxy6upyn2w8ryau3fvcuaywgncvz89verfyy24vverl9pw2h5uwv30aq9qm6xj7s',
+    amount: 360000,
+  },
+  ttl: '0x641a5',
+};
+
 function CoinAda(props: Props) {
   const ada = new ADA();
   const [address, setAddress] = useState('');
+  const [transferSize, setTransferSize] = useState('');
+  const [transferTx, setTransferTx] = useState('');
   const [signedTransaction, setSignedTransaction] = useState('');
   const [value, setValue] = useState('0');
   const [to, setTo] = useState('0x81bb32e4A7e4d0500d11A52F3a5F60c9A6Ef126C');
@@ -48,23 +70,32 @@ function CoinAda(props: Props) {
     }, setAddress);
   };
 
+  const getTransferSize = async () => {
+    handleState(async () => {
+      const size = await ada.getTransactionSize(txWithoutFee);
+      console.log('size :', size);
+      return size.toString();
+    }, setTransferSize);
+  };
+
   const signTransaction = async () => {
     handleState(async () => {
       const transaction = {
-        chainId: 1,
-        nonce: '0x289',
-        gasPrice: '0x20c855800',
-        gasLimit: '0x520c',
-        to: to,
-        value: `0x${parseInt(value).toString(16)}`,
-        data: '',
+        fee: 0x28d05,
+        ...txWithoutFee
       };
 
       const appId = localStorage.getItem('appId');
       if (!appId) throw new Error('No Appid stored, please register!');
-      const signedTx = await ada.signTransaction(transport!, appPrivateKey, appId, 0, transaction);
+      const options = {
+        transport: transport!,
+        appPrivateKey,
+        appId
+      };
+
+      const signedTx = await ada.signTransaction(transaction, options);
       return signedTx;
-    }, setSignedTransaction);
+    }, setTransferTx);
   };
 
   return (
@@ -78,8 +109,20 @@ function CoinAda(props: Props) {
         onClick={getAddress}
         disabled={disabled}
       />
+      <NoInput
+        title='Get Transfer Size'
+        content={transferSize}
+        onClick={getTransferSize}
+        disabled={disabled}
+      />
+      <NoInput
+        title='Sign Transfer'
+        content={transferTx}
+        onClick={signTransaction}
+        disabled={disabled}
+      />
       <TwoInputs
-        title='Sign Transaction'
+        title='not used'
         content={signedTransaction}
         onClick={signTransaction}
         disabled={disabled}
--- packages/coin-tester/src/components/coins/index.ts
+++ packages/coin-tester/src/components/coins/index.ts
@@ -1,9 +1,9 @@
 import template from './template';
 import ada from './ada';
-import cro from './coin-cro';
+// import cro from './coin-cro';
 
 export default [
   { path: 'template', Element: template },
   { path: 'ada', Element: ada },
-  { path: 'cro', Element: cro},
+ //  { path: 'cro', Element: cro},
 ];
